acfa1611c27e089ff7721a70267dce3ed74e9d83
Anshuman Bisoyi <ansuman746@gmail.com>
2022-01-30 21:20:03 +0530

    intial

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,52 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
-void main() => runApp(
-      MaterialApp(
-        home: null,
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+          backgroundColor: Colors.blue[300],
+          appBar: AppBar(
+            title: Text(
+              'Ask Me Anything',
+              style: TextStyle(color: Colors.white),
+            ),
+            backgroundColor: Colors.blue[900],
+          ),
+          body: BallPage()),
+    ),
+  );
+}
+
+class BallPage extends StatefulWidget {
+  @override
+  _BallPageState createState() => _BallPageState();
+}
+
+class _BallPageState extends State<BallPage> {
+  var ballNumber = 1;
+  void getBallNumber() {
+    setState(() {
+      ballNumber = Random().nextInt(5) + 1;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: TextButton(
+              onPressed: () {
+                getBallNumber();
+                print("Ball got clicked.");
+              },
+              child: Image.asset('images/ball$ballNumber.png'),
+            ),
+          ),
+        ],
       ),
     );
+  }
+}
